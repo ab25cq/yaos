476601b7a1d7d7ffe2be9061f4a4b16290fc4a11
ab25cq <ab25cq@icloud.com>
2024-12-10 00:48:50 +0900

    ii

--- update.sh
+++ update.sh
@@ -4,6 +4,9 @@ export EDITOR=vin
 git config --global user.email ab25cq@icloud.com
 git config --global user.name ab25cq
 
+rm -rf bootloader.efi uefi.img bootloader.c.o
+
+
 git add .
 git commit 
 git remote set-url origin git@github.com:ab25cq/yaos.git
